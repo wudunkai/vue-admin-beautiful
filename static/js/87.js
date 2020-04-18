56e41882bc7095f2f9d7be3ce886c5773fb3d8ae
初志鑫 <1204505056@qq.com>
2020-04-18 10:08:03 +0800

    deploy

---
+++ static/js/87.js
@@ -0,0 +1,34 @@
+/*!
+ * byui前端敏捷开发平台 : vue-admin-beautiful
+ *  copyright:1204505056@qq.com 
+ *  author: chuzhixin <1204505056@qq.com> 
+ *  participants: 
+ *  time: "2020-4-18 10:7:33"
+ */
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[87],{
+
+/***/ "./src/plugins/byuiPlayer.js":
+/*!***********************************!*\
+  !*** ./src/plugins/byuiPlayer.js ***!
+  \***********************************/
+/*! exports provided: ByuiPlayerMp4, ByuiPlayerHls, ByuiPlayerFlv, ByuiPlayerCustom */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "ByuiPlayerMp4", function() { return ByuiPlayerMp4; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "ByuiPlayerHls", function() { return ByuiPlayerHls; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "ByuiPlayerFlv", function() { return ByuiPlayerFlv; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "ByuiPlayerCustom", function() { return ByuiPlayerCustom; });
+/* harmony import */ var zx_player__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! zx-player */ "./node_modules/_zx-player@0.9.5@zx-player/src/components/export.js");
+
+var ByuiPlayerMp4 = zx_player__WEBPACK_IMPORTED_MODULE_0__["Mp4"];
+var ByuiPlayerHls = zx_player__WEBPACK_IMPORTED_MODULE_0__["Hls"];
+var ByuiPlayerFlv = zx_player__WEBPACK_IMPORTED_MODULE_0__["Flv"];
+var ByuiPlayerCustom = zx_player__WEBPACK_IMPORTED_MODULE_0__["Custom"];
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=87.js.map
